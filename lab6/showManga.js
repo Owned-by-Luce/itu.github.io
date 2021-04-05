8c8e90b0b16a438fbde8dbdb1193060ad69ca80a
Bayarkhuu <bayarkhuu.srs@yahoo.com>
2021-04-05 16:56:13 +0800

    lab6

--- lab6/showManga.js
+++ lab6/showManga.js
@@ -1,4 +1,4 @@
-let SERVICE = "http://localhost:9000/itu.github.io";
+let SERVICE = "https://owned-by-luce.github.io/itu.github.io";
 let table = document.querySelector(".table");
 let uuid = window.location.hash.replace("#", "");
 let base64Flag = "data:image/png;base64,"
