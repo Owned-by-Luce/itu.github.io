f4f71b0a82eb19431a88f5a9aea9f10986f59ef7
Bayarkhuu <bayarkhuu.srs@yahoo.com>
2021-04-24 17:07:01 +0800

    lab8

--- lab8/hangmanController.js
+++ lab8/hangmanController.js
@@ -10,27 +10,37 @@ let questions =
 let answers =
     {
         "0": ["ногоон", "nogoon"],
-        "1": ["3"],
+        "1": ["3", "гурав", "gurav"],
         "2": ["элс", "els"],
-        "3": ["21"],
+        "3": ["21", "хорин нэг", "khorin neg", "horin neg"],
         "4": ["лондон", "london"]
     }
 
 let index = 0;
+let second = 10;
 let livesContainer = document.querySelector(".lives-container");
 let dialog = document.querySelector(".dialog");
 let dialogImage = document.querySelector("#dialog-image");
 let dialogMsg = document.querySelector("#dialog-msg");
+let valueInput = document.querySelector("#value");
+let submit = document.querySelector(".submit");
 reset();
+startTime();
 
-function next() {
-    let value = document.querySelector("#value").value.toLowerCase();
-    let i = answers[index].filter(f => f === value).length;
+valueInput.addEventListener("keyup", e => {
+    if (e.keyCode === 13) {
+        submit.click();
+    }
+})
+
+function next(add) {
+    let value = valueInput.value.toLowerCase();
+    let i = answers[!add ? index - 1 : index].filter(f => f === value).length;
 
     if (i !== 0) {
         index++;
         document.querySelector("#question").textContent = questions[index];
-        document.querySelector("#value").value = "";
+        valueInput.value = "";
         if (index === 5) {
             //Ялсан
             dialog.style.display = "block";
@@ -38,23 +48,37 @@ function next() {
             dialogMsg.textContent = "Та хожлоо";
         }
     } else {
-        console.log()
         if (livesContainer.children.length > 1) {
-            document.querySelector("#value").value = "";
-            livesContainer.removeChild(livesContainer.lastElementChild)
+            valueInput.value = "";
+            livesContainer.removeChild(livesContainer.lastElementChild);
         } else {
-            livesContainer.removeChild(livesContainer.lastElementChild)
+            livesContainer.removeChild(livesContainer.lastElementChild);
             //Ялагдсан
-            dialogImage.src = "../icons/lose.png"
+            dialogImage.src = "../icons/lose.png";
             dialogMsg.textContent = "Та хожигдлоо";
             dialog.style.display = "block";
         }
     }
+    second = 10;
+}
+
+function startTime() {
+    return setInterval(() => {
+        submit.textContent = `Шалгах /${second}/`;
+        if (second === 0) {
+            index++;
+            document.querySelector("#question").textContent = questions[index];
+            next(false);
+        }
+        second--;
+    }, 1000);
 }
 
 function reset() {
+    valueInput.value = "";
     dialog.style.display = "none";
     index = 0;
+    second = 10;
     document.querySelector("#question").textContent = questions[index];
     livesContainer.innerHTML = "";
     livesContainer.insertAdjacentHTML("beforeend", `
@@ -63,5 +87,5 @@ function reset() {
         <img class="lives" id="3" src="../images/heart.png" alt="heart">
         <img class="lives" id="4" src="../images/heart.png" alt="heart">
         <img class="lives" id="5" src="../images/heart.png" alt="heart">
-`);
+    `);
 }
